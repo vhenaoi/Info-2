4a612d72bcfa8d0182439f41549b83d12500267c
vhenaoi <vhenao.isaza@gmail.com>
2023-02-02 23:20:53 -0500

    herencia

---
+++ herencia.py
@@ -0,0 +1,35 @@
+class ave:
+
+    def __init__(self,tipo,vuela):
+        self.ave =  tipo
+        self.vuelo = vuela
+        self._oviparos = True
+        self.pico = True
+
+    def comer(self,comida):
+        print("Este tipo de ave come normalmente : ", comida)
+
+    def volar(self):
+        print("Este tipo de ave puede volar : ", self.vuelo)
+
+class ganso(ave):
+
+    def __init__(self,tipo,vuela,accion,pata):
+        ave.__init__(self,tipo,vuela)
+        self.habilidad = accion
+        self.patas = pata
+
+    def destreza(self):
+        print("Esta ave puede : ", self.habilidad)
+
+class pato(ganso):
+    pass
+class gallina(ave):
+    pass
+
+p=ave("gallina",False)
+p.volar()
+p.comer("Maiz")
+
+g=ganso("carnivoro",True,"nadar","4")
+g.destreza()
---
+++ herencia_anima.py
@@ -0,0 +1,26 @@
+class Animal:
+    def __init__(self, especie, edad):
+        self.especie = especie
+        self.edad = edad
+
+    # Método genérico pero con implementación particular
+    def hablar(self):
+        # Método vacío
+        pass
+
+    # Método genérico pero con implementación particular
+    def moverse(self):
+        # Método vacío
+        pass
+
+    # Método genérico con la misma implementación
+    def describeme(self):
+        print("Soy un Animal del tipo", type(self).__name__)
+
+# Perro hereda de Animal
+class Perro(Animal):
+    pass
+
+mi_perro = Perro('mamífero', 10)
+mi_perro.describeme()
+# Soy un Animal del tipo Perro
